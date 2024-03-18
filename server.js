5bbea2ce75daaec431bfaabc2a3dcd2e9c5b75b0
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2024-03-18 22:25:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+//important 
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 2121
@@ -8,23 +9,39 @@ require('dotenv').config()
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
+//dbname is replaced with 'todo', why? I don't know yet
+//if it's successful, we connect to 'todo' database
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
+    //converting the objects from out 
     })
     
 app.set('view engine', 'ejs')
+// telling the server what to respond with, ejs is a html style file within the server
 app.use(express.static('public'))
+    //what are statis files? servered direcltly to clients without server
+// sends files without modifications, as is
+// telling express to use files in public folder, static files
 app.use(express.urlencoded({ extended: true }))
+        //tells express.js to handle URL-encoded data
 app.use(express.json())
+    //When you ask for data, like getting names of drinks or Pokémon, the server sends it back in JSON because it's easy for both computers and people to understand.
+    
 
 
 app.get('/',async (request, response)=>{
     const todoItems = await db.collection('todos').find().toArray()
+        //it's the name of a collection within the database. In MongoDB, a database is a container for collections, and collections are like tables in a relational database. They contain the actual documents (records) you're working with. In your case, db.collection('todos') refers to the todos collection within your todo database, where each document represents a to-do item.
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+// send back the items not com
     response.render('index.ejs', { items: todoItems, left: itemsLeft })
+// items that were not checked off and done
+    .catch(error => console.error(error))
+    //set up a tri-catch
+    
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -43,6 +60,8 @@ app.post('/addTodo', (request, response) => {
     })
     .catch(error => console.error(error))
 })
+    //adding new 
+
 
 app.put('/markComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
